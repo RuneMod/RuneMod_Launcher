17d642ab4bed8297970e3d0c2f156171fe400c7b
RuneMod <RuneMod@protonmail.com>
2024-11-30 10:32:19 +0000

    .

--- out/production/RuneMod_Launcher/RuneMod_Launcher.bat
+++ out/production/RuneMod_Launcher/RuneMod_Launcher.bat
@@ -3,24 +3,30 @@ chdir /d %temp%
 
 set /p localVersion=<%temp%\localVersion.txt
 
-echo echo localVersion: %localVersion%
+echo localVersion: %localVersion%
 
-echo gettings latest versionNumber...
+echo getting latest versionNumber...
 curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://runemod.net/downloadables/launcher/version.txt" -o latestVersion.txt
 
-set /p latestVersion=<%temp%\latestVersion.txt
-echo echo latestVersion: %latestVersion%
-
-If %latestVersion% == %localVersion% (echo rm plugin version is up to date!) ELSE (
-echo:
-echo rm plugin updating to version %latestVersion%...
-curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://runemod.net/downloadables/launcher/runemod-all.jar" -o runemod-all.jar
-
-rem set local version now that latest version has been downloaded.
-
-(echo %latestVersion%) > %temp%\localVersion.txt
+if errorlevel 1 (
+    echo Unable to reach the runemod webserver. Please notify RuneFace of the issue.
+    echo Press any key to continue...
+    pause >nul
+    exit
 )
 
-java -jar -ea %temp%\runemod-all.jar --developer-mode
+set /p latestVersion=<%temp%\latestVersion.txt
+echo latestVersion: %latestVersion%
 
-pause
+If %latestVersion% == %localVersion% (
+    echo rm plugin version is up to date!
+) ELSE (
+    echo:
+    echo rm plugin updating to version %latestVersion%...
+    curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://runemod.net/downloadables/launcher/runemod-all.jar" -o runemod-all.jar
+
+    rem set local version now that latest version has been downloaded.
+    (echo %latestVersion%) > %temp%\localVersion.txt
+)
+
+java -jar -ea %temp%\runemod-all.jar --developer-mode >>log_debug.txt
